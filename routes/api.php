8d43faa446883411d391572ce022320244422d01
Mihai <mihai.ocneanu@gmail.com>
2020-10-07 17:29:20 +0300

    updates routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use LaravelEnso\PackagingUnits\Http\Controllers\Packaging\Create;
+use LaravelEnso\PackagingUnits\Http\Controllers\Packaging\Destroy;
+use LaravelEnso\PackagingUnits\Http\Controllers\Packaging\Edit;
+use LaravelEnso\PackagingUnits\Http\Controllers\Packaging\ExportExcel;
+use LaravelEnso\PackagingUnits\Http\Controllers\Packaging\InitTable;
+use LaravelEnso\PackagingUnits\Http\Controllers\Packaging\Options;
+use LaravelEnso\PackagingUnits\Http\Controllers\Packaging\Store;
+use LaravelEnso\PackagingUnits\Http\Controllers\Packaging\TableData;
+use LaravelEnso\PackagingUnits\Http\Controllers\Packaging\Update;
 
 Route::middleware(['api', 'auth', 'core'])
-    ->namespace('LaravelEnso\PackagingUnits\Http\Controllers\Packaging')
     ->prefix('api/administration/packagingUnits')
     ->as('administration.packagingUnits.')
     ->group(function () {
-        Route::get('create', 'Create')->name('create');
-        Route::post('', 'Store')->name('store');
-        Route::get('{packagingUnit}/edit', 'Edit')->name('edit');
-        Route::patch('{packagingUnit}', 'Update')->name('update');
-        Route::delete('{packagingUnit}', 'Destroy')->name('destroy');
-        Route::get('initTable', 'InitTable')->name('initTable');
-        Route::get('tableData', 'TableData')->name('tableData');
-        Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
-        Route::get('options', 'Options')->name('options');
+        Route::get('create', Create::class)->name('create');
+        Route::post('', Store::class)->name('store');
+        Route::get('{packagingUnit}/edit', Edit::class)->name('edit');
+        Route::patch('{packagingUnit}', Update::class)->name('update');
+        Route::delete('{packagingUnit}', Destroy::class)->name('destroy');
+        Route::get('initTable', InitTable::class)->name('initTable');
+        Route::get('tableData', TableData::class)->name('tableData');
+        Route::get('exportExcel', ExportExcel::class)->name('exportExcel');
+        Route::get('options', Options::class)->name('options');
     });
